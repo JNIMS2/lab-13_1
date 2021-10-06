c163f3dec4c95ea774bda26c4b8d7fe092b8a32b
Jessica Miller-Nims <Jessicamiller-nims@quickenloans.com>
2021-10-05 21:37:05 -0400

    first commit

---
+++ Lab16_1/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Lab16_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab16_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult GetForm()
+        {
+            return View();
+        }
+        //im gonna figure this use form out in a bit
+        public IActionResult UseForm() 
+        {
+            return Content("Please enter in all fields");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
